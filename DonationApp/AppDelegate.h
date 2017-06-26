5f3c31df9b4654c23f02010d10af37e1667da9ae
Alex Lee <x.lee@mail.utoronto.ca>
2017-06-26 12:06:25 -0700

    Initial commit

---
+++ DonationApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DonationApp
+//
+//  Created by Alex Lee on 2017-06-26.
+//  Copyright © 2017 Alex Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DonationApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DonationApp
+//
+//  Created by Alex Lee on 2017-06-26.
+//  Copyright © 2017 Alex Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
