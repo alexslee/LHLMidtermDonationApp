3c483804bd47b5155a6db6da1401a06e42a3c7a9
Fernando Jinzenji <fernandoj@me.com>
2017-06-26 19:45:22 -0700

    Added mapview and photopicker in add item view controller

--- DonationApp/AddItemViewController.m
+++ DonationApp/AddItemViewController.m
@@ -8,9 +8,10 @@
 
 #import "AddItemViewController.h"
 #import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
 #import "SelectCategoryViewController.h"
 
-@interface AddItemViewController () <SelectCategoryViewControllerDelegate>
+@interface AddItemViewController () <SelectCategoryViewControllerDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CLLocationManagerDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *titleTextField;
 @property (weak, nonatomic) IBOutlet UITextField *categoryTextField;
@@ -18,6 +19,12 @@
 @property (weak, nonatomic) IBOutlet UIImageView *image1;
 @property (weak, nonatomic) IBOutlet UIImageView *image2;
 @property (weak, nonatomic) IBOutlet UIImageView *image3;
+@property (weak, nonatomic) IBOutlet UIView *photoMenuView;
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+@property (nonatomic, strong) UIImagePickerController *imagePicker;
+@property (nonatomic, strong) CLLocationManager *locationManager;
+@property (nonatomic, strong) CLLocation *currentLocation;
 
 @end
 
@@ -26,13 +33,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    // setup image picker
+    self.imagePicker = [[UIImagePickerController alloc] init];
+    self.imagePicker.delegate = self;
     
+    // setup location manager
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+//    self.locationManager.allowsBackgroundLocationUpdates = YES;
+    [self.locationManager requestWhenInUseAuthorization];
+    [self.locationManager requestLocation];
+
 }
 
 #pragma mark - Navigation
 
 - (IBAction)saveButtonPressed:(UIBarButtonItem *)sender {
 }
+
+- (IBAction)addPhotoButtonPressed:(UIButton *)sender {
+    self.photoMenuView.hidden = NO;
+}
+
+- (IBAction)photoMenuButtonPressed:(UIButton *)sender {
+    
+    if([sender.currentTitle isEqual:@"Camera"])
+    {
+        self.imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        [self presentViewController:self.imagePicker animated:YES completion:nil];
+    }
+    else if([sender.currentTitle isEqual:@"Photo Library"])
+    {
+        self.imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        [self presentViewController:self.imagePicker animated:YES completion:nil];
+    }
+    self.photoMenuView.hidden = YES;
+}
+
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
@@ -41,6 +78,41 @@
         SelectCategoryViewController *controller = (SelectCategoryViewController *)segue.destinationViewController;
         controller.delegate = self;
     }
+}
+
+#pragma mark - Location
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
+    
+    if (!self.currentLocation) {
+        self.currentLocation = locations[0];
+        
+        NSLog(@"location: %@", locations[0]);
+        
+        int regionRadius = 1000;
+        CLLocationCoordinate2D startLocationCoordinate = self.currentLocation.coordinate;
+        MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(startLocationCoordinate, regionRadius*2, regionRadius*2);
+        [self.mapView setRegion:region];
+
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    NSLog(@"Error getting the location updates: %@", error.localizedDescription);
+}
+
+#pragma mark - ImagePicker delegate
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [self dismissViewControllerAnimated:NO completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info
+{
+    UIImage *selectedImage = [info objectForKey:UIImagePickerControllerOriginalImage];
+    self.image1.image = selectedImage;
+    [self dismissViewControllerAnimated:NO completion:nil];
 }
 
 #pragma mark - SelectCategoryViewControllerDelegate
